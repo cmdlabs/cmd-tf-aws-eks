6119381a349ac869cf972b4910464a2766fcf2a5
Paul Gear <paul.gear@cmd.com.au>
2021-07-01 13:07:26 +1000

    Form lists in a terraform-1.x-compatible way
    I'm not sure what the minimum version requirement for this construct is.

--- nodegroup/node.tf
+++ nodegroup/node.tf
@@ -5,7 +5,7 @@ resource "aws_autoscaling_group" "node" {
   desired_capacity    = var.asg_desired_capacity
   min_size            = var.asg_min_size
   max_size            = var.asg_max_size
-  vpc_zone_identifier = var.asg_per_subnet ? list(element(var.vpc_subnets, count.index)) : var.vpc_subnets
+  vpc_zone_identifier = var.asg_per_subnet ? [element(var.vpc_subnets, count.index)] : var.vpc_subnets
 
   suspended_processes = var.suspended_processes
   enabled_metrics     = var.enabled_metrics
